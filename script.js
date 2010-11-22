67d5a7e107a296057a9e8112dad216620dd30fff
jgv <jvingiano@gmail.com>
2010-11-22 10:28:29 -0500

    checking for jquery

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-var e = document.createElement("script");
-e.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js";
-e.type = "text/javascript";
-document.getElementsByTagName("head")[0].appendChild(e);
+if (jQuery) {
 
-var deal = jQuery.noConflict();
-var width = deal(window).width(); 
-var height = deal(widow).width();
+    var deal = jQuery.noConflict();
+    var width = deal(window).width(); 
+    var height = deal(widow).width();
+    
+    console.log(widtth + ' ' + height);
 
-console.log(widtth + ' ' + height);
+
+} else {
+
+    var e = document.createElement("script");
+    e.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js";
+    e.type = "text/javascript";
+    document.getElementsByTagName("head")[0].appendChild(e);
+    
+    deal(document).ready(function(){
+        var deal = jQuery.noConflict();
+        var width = deal(window).width(); 
+        var height = deal(widow).width();
+        
+        console.log(widtth + ' ' + height);
+    });
+}
+
