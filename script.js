39bf5a974610dbf092660f6e06f5e7c8f9f04d2f
jgv <jvingiano@gmail.com>
2010-11-22 11:29:11 -0500

    adding css

--- script.js
+++ script.js
@@ -28,17 +28,25 @@ deal(document).ready(function(){
     var glasses_top = (height / 2) - glasses_h;
     var glasses_left = (width / 2) - glasses_w;
 
+    var css = document.createElement("link");
+			css.setAttribute("href", "https://github.com/jgv/deal-with-it/raw/master/deal.css");
+			css.setAttribute("rel", "stylesheet");
+			css.setAttribute("type", "text/css");
+			document.getElementsByTagName("head")[0].appendChild(css);
+
     var img = document.createElement("div");
     img.id = "deal_with_it";
     var html = "<img width='" + glasses_w + "'";
     html += "height='" + glasses_h + "'";
-    html += "style='z-index:999999;position:absolute; top:" + glasses_top + "; left:" + glasses_left + "'";
+    html += "style='" + glasses_top + "; left:" + glasses_left + "'";
     html += "src='https://github.com/jgv/deal-with-it/raw/master/glasses.png'>";
 //    img += "</div>";
 
     img.innerHTML = html; 
     document.getElementsByTagName("body")[0].appendChild(img);
-
+    var deal_div = deal("#deal_with_it");
+    deal_div.css({ "top" : glasses_top, "left" : glasses_left } );
+    
 });
 
 
